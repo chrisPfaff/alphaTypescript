070c404e37a5c4bee6e2ceb1d740ba2518536fe1
Chris Pfaff <chrispfaff10@gmail.com>
2022-08-09 19:04:07 -0500

    basic app looking for api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
+import './App.scss';
+
+import Header from './components/Header'
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  fetch(`https://api.artic.edu/api/v1/artworks?page=2&limit=100`)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import "../styles/header.scss";
+
+import React from 'react'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <>
+    <header>
+      <h1>Test</h1>
+    </header>
+    </>
+  )
+}
+
+export default Header
