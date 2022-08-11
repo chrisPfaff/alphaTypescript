47355635abbf857683cb3e5a2e399e419d5ac94f
Chris Pfaff <chrispfaff10@gmail.com>
2022-08-10 21:39:28 -0500

    main page changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import './App.scss';
 
-import Header from './components/Header'
+import Header from './components/Header';
+import HeroImage from './components/HeroImage';
 import React from 'react';
-import { useState } from 'react';
 
 function App() {
-  fetch(`https://api.artic.edu/api/v1/artworks?page=2&limit=100`)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-
   return (
     <div className="App">
       <Header/>
+      <HeroImage/>
     </div>
   );
 }
